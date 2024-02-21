421adf9722fa2821ae11ed222acebcfed6fc4a13
Iván Villegas Pérez <89591182+ivanvillegas7@users.noreply.github.com>
2024-02-21 12:09:03 +0100

    Add files via upload

---
+++ Python/Milestone2.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 21 12:05:37 2024
+
+@author: Iván Villegas Pérez
+"""
+
---
+++ Python/Milestone3.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 21 12:06:38 2024
+
+@author: Iván Villegas Pérez
+"""
+
