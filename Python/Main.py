c248994192d851fdb9be9cb92331cfeabbbdeeb2
Iván Villegas Pérez <89591182+ivanvillegas7@users.noreply.github.com>
2024-03-27 21:39:46 +0100

    Update Main.py

--- Python/Main.py
+++ Python/Main.py
@@ -42,7 +42,7 @@ def main():
     #Run the code related to solving the background cosmology of the Universe
     #and show the results related to the supernove fitting.
     
-    #Milestone1.milestone1()
+    Milestone1.milestone1()
     
     #Print a message saying that the execution of the code is over.
     
